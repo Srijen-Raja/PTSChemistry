877f2588b7f342051977dc6c0f37e20e4f51dd24
srije <srijen222@gmail.com>
2025-09-19 20:26:43 +0530

    added crash course options 2

--- lib/Options.dart
+++ lib/Options.dart
@@ -7,26 +7,23 @@ class OptionWithLink {
 }
 
 List<OptionWithLink> options = [
-  OptionWithLink('Day 1: Some Basic Concepts of Chemistry', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 2: States of Matter', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 3: Atomic Structure', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 4: Chemical Bonding and Molecular Structure', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 5: Unit Test 1 (General Chemistry)', 'https://ptschemistryclass.vercel.app/'),
-
-  OptionWithLink('Day 6: Chemical Thermodynamics', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 7: Thermochemistry', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 8: Solutions', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 9: Physical and Chemical Equilibrium', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 10: Ionic Equilibrium', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 11: Unit Test 2 (Physical Chemistry-I)', 'https://ptschemistryclass.vercel.app/'),
-
-  OptionWithLink('Day 12: Redox Reactions', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 13: Electrochemistry', 'https://ptschemistryclass.vercel.app/'),
-  OptionWithLink('Day 14: Chemical Kinetics', 'https://ptschemistryclass.vercel.app/'),
+  OptionWithLink('Day 1: Some Basic Concepts of Chemistry', 'https://forms.office.com/r/YkrCsCTZ4D?origin=lprLink'),
+  OptionWithLink('Day 2: States of Matter', 'https://forms.office.com/r/NVUPsnQjME?origin=lprLink'),
+  OptionWithLink('Day 3: Atomic Structure', 'https://forms.office.com/r/GsHsvuntJM?origin=lprLink'),
+  OptionWithLink('Day 4: Chemical Bonding and Molecular Structure', 'https://forms.office.com/r/0tku6HPKg1?origin=lprLink'),
+  OptionWithLink('Day 5: Unit Test 1 (General Chemistry)', 'https://forms.office.com/r/nrVkgUcqEn?origin=lprLink'),
+  OptionWithLink('Day 6: Chemical Thermodynamics', 'https://forms.office.com/r/xCikTVciy9?origin=lprLink'),
+  OptionWithLink('Day 7: Thermochemistry', 'https://forms.office.com/r/PJ01b5exnF?origin=lprLink'),
+  OptionWithLink('Day 8: Solutions', 'https://forms.office.com/r/tivdTX1H0e?origin=lprLink'),
+  OptionWithLink('Day 9: Physical and Chemical Equilibrium', 'https://forms.office.com/r/9nXPfXYzjF?origin=lprLink'),
+  OptionWithLink('Day 10: Ionic Equilibrium', 'https://forms.office.com/r/cVWNeV2pNM?origin=lprLink'),
+  OptionWithLink('Day 11: Unit Test 2 (Physical Chemistry-I)', 'https://forms.office.com/r/pF9Q5JP6xV?origin=lprLink'),
+  OptionWithLink('Day 12: Redox Reactions', 'https://forms.office.com/r/UcZBVy8Dgk?origin=lprLink'),
+  OptionWithLink('Day 13: Electrochemistry', 'https://forms.office.com/r/2yz9EEuahh?origin=lprLink'),
+  OptionWithLink('Day 14: Chemical Kinetics', 'https://forms.office.com/r/fqVnK1vXnF?origin=lprLink'),
   OptionWithLink('Day 15: Adsorption and Catalysis', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 16: Colloidal State', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 17: Unit Test 3 (Physical Chemistry-II)', 'https://ptschemistryclass.vercel.app/'),
-
   OptionWithLink('Day 18: Classification and Periodicity of Elements', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 19: General Principles and Processes of Isolation of Metals', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 20: Hydrogen', 'https://ptschemistryclass.vercel.app/'),
@@ -35,7 +32,6 @@ List<OptionWithLink> options = [
   OptionWithLink('Day 23: The d- and f-Block Elements', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 24: Coordination Compounds', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 25: Unit Test 4 (Inorganic Chemistry)', 'https://ptschemistryclass.vercel.app/'),
-
   OptionWithLink('Day 26: Environmental Chemistry', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 27: General Organic Chemistry', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 28: Hydrocarbons', 'https://ptschemistryclass.vercel.app/'),
@@ -43,15 +39,12 @@ List<OptionWithLink> options = [
   OptionWithLink('Day 30: Organic Compounds Containing Oxygen', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 31: Organic Compounds Containing Nitrogen', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 32: Unit Test 5 (Organic Chemistry-I)', 'https://ptschemistryclass.vercel.app/'),
-
   OptionWithLink('Day 33: Polymers', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 34: Biomolecules', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 35: Chemistry in Everyday Life', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 36: Analytical Chemistry', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 37: Unit Test 6 (Organic Chemistry-II)', 'https://ptschemistryclass.vercel.app/'),
-
   OptionWithLink('Day 38: Mock Test 1', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 39: Mock Test 2', 'https://ptschemistryclass.vercel.app/'),
   OptionWithLink('Day 40: Mock Test 3', 'https://ptschemistryclass.vercel.app/'),
 ];
-
